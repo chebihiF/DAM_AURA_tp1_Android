2962a89841ed8f992b5fcfbe4ea16b8ba49b4dca
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-08-31 09:18:01 +0100

    add timer

--- app/src/main/java/org/m2i/tp1/MainActivity.java
+++ app/src/main/java/org/m2i/tp1/MainActivity.java
@@ -3,6 +3,7 @@ package org.m2i.tp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,12 +13,14 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Random random = new Random(); // class random => generer nombres aleatoires ...
-    TextView nbr1_txt, nbr2_txt, score_txt;
+    TextView nbr1_txt, nbr2_txt, score_txt, timer_txt;
     Button resp1_btn, resp2_btn, resp3_btn, start_btn;
+
+    CountDownTimer timer ;
 
     boolean game_started = false ;
 
-    int score = 0 ;
+    int score = 0, timer_unit = 5 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         resp3_btn =findViewById(R.id.resp3);
         score_txt = findViewById(R.id.score);
         start_btn = findViewById(R.id.start);
+        timer_txt = findViewById(R.id.timer_txt);
+
 
         resp1_btn.setOnClickListener(this);
         resp2_btn.setOnClickListener(this);
@@ -49,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void start_game(){
+
+        setTimer();
+        timer_txt.setText(""+timer_unit);
 
         int n1 = random.nextInt(200); // générer un nombre aleatoire entre 0 - 200
         int n2 = random.nextInt(200); // générer un nombre aleatoire entre 0 - 200
@@ -80,6 +88,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         if(game_started) {
 
+            timer.cancel();
+            timer_unit = 5 ;
+
             Button btn = (Button) v;
             int resp = Integer.parseInt(btn.getText().toString());
             int nbr1 = Integer.parseInt(nbr1_txt.getText().toString());
@@ -97,5 +108,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             start_game();
         }
+    }
+
+    public void setTimer(){
+        timer = new CountDownTimer(5000,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timer_unit -- ;
+                timer_txt.setText(""+timer_unit);
+            }
+
+            @Override
+            public void onFinish() {
+                timer_unit = 5 ;
+                score -- ;
+                score_txt.setText("Score : " + score);
+                if(score > 0)
+                    score_txt.setTextColor(getResources().getColor(R.color.my_green));
+                else if (score < 0)
+                    score_txt.setTextColor(getResources().getColor(R.color.my_red));
+                start_game();
+            }
+        };
+
+        timer.start();
     }
 }
